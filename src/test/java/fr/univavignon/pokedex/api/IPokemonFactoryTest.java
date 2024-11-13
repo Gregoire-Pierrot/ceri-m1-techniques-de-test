0cd2f8f8cdfd4b2385ad529bec4773243b1b49b9
Grégoire <gregpierr453@gmail.com>
2024-11-13 10:58:49 +0100

    Essaie couverture 100%

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -2,6 +2,10 @@ package fr.univavignon.pokedex.api;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -9,6 +13,9 @@ import org.mockito.Mockito;
 public class IPokemonFactoryTest {
 
     private IPokemonFactory iPokemonFactory_mocked;
+    private Pokemon Bulbizarre = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, 56);
+    private Pokemon Charmender = new Pokemon(3, "Charmender", 126, 126, 90, 7813, 64, 4000, 4, 56);
+    private Pokemon Aquali = new Pokemon(133, "Aquali", 186, 168, 260, 2729, 202, 5000, 4, 100);
     
     @Before
     public void setUp(){
@@ -19,10 +26,35 @@ public class IPokemonFactoryTest {
     public void testCreatePokemon(){
         Pokemon pokemonTest = new Pokemon(-1, "PokemonTest", 0, 0, 0, 613, 64, 4000, 4, 56);
         Mockito.when(iPokemonFactory_mocked.createPokemon(-1, 613, 64, 4000, 4)).thenReturn(pokemonTest);
-        assertEquals(pokemonTest.getIndex(), -1);
-        assertEquals(pokemonTest.getCp(), 613);
-        assertEquals(pokemonTest.getHp(), 64);
-        assertEquals(pokemonTest.getDust(), 4000);
-        assertEquals(pokemonTest.getCandy(), 4);
+        assertEquals(-1, iPokemonFactory_mocked.createPokemon(-1, 613, 64, 4000, 4).getIndex());
+        assertEquals(613, iPokemonFactory_mocked.createPokemon(-1, 613, 64, 4000, 4).getCp());
+        assertEquals(64, iPokemonFactory_mocked.createPokemon(-1, 613, 64, 4000, 4).getHp());
+        assertEquals(4000, iPokemonFactory_mocked.createPokemon(-1, 613, 64, 4000, 4).getDust());
+        assertEquals(4, iPokemonFactory_mocked.createPokemon(-1, 613, 64, 4000, 4).getCandy());
+        assertEquals(56, iPokemonFactory_mocked.createPokemon(-1, 613, 64, 4000, 4).getIv());
+
+        List<Pokemon> pokemonList = new ArrayList<Pokemon>();
+        pokemonList.add(Bulbizarre);
+        pokemonList.add(Charmender);
+        pokemonList.add(Aquali);
+
+        List<Pokemon> pokemonList_Name = new ArrayList<Pokemon>();
+        pokemonList_Name.add(Aquali);
+        pokemonList_Name.add(Bulbizarre);
+        pokemonList_Name.add(Charmender);
+
+        List<Pokemon> pokemonList_Index = pokemonList;
+
+        List<Pokemon> pokemonList_CP = new ArrayList<Pokemon>();
+        pokemonList_CP.add(Charmender);
+        pokemonList_CP.add(Aquali);
+        pokemonList_CP.add(Bulbizarre);
+
+        Collections.sort(pokemonList, PokemonComparators.NAME);
+        assertEquals(pokemonList_Name, pokemonList);
+        Collections.sort(pokemonList, PokemonComparators.INDEX);
+        assertEquals(pokemonList_Index, pokemonList);
+        Collections.sort(pokemonList, PokemonComparators.CP);
+        assertEquals(pokemonList_Name, pokemonList);
     }
 }
