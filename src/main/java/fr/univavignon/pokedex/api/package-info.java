c641ecf28cf716869daffdc10b906ec77ae0dea9
Grégoire Pierrot <gregpierr453@gmail.com>
2024-12-19 00:39:16 +0100

    Ajout de package-info.java

---
+++ src/main/java/fr/univavignon/pokedex/api/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ * Ce package contient les interfaces et les classes liées à la gestion et création du Pokédex, des Pokémons et des dresseurs.
+ * 
+ * @version 1.0
+ * @author Pierrot Grégoire
+ */
+package fr.univavignon.pokedex.api;
